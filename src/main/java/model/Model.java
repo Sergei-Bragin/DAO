508e7ea6eba670e6c839e36356ce2ae53e455d77
Sergei Bragin <serbragin1@gmail.com>
2015-12-21 22:16:55 +0200

    Initial commit

---
+++ src/main/java/model/Model.java
@@ -0,0 +1,23 @@
+package model;
+
+import java.io.Serializable;
+
+public class Model implements Serializable {
+    private Long id;
+
+    public Model() {
+
+    }
+
+    public Model(Long id){
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
